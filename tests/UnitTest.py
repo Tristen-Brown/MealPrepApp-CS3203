d275a03e1be355f5b7b2a71111ba24ded250679a
Tristen-Brown <143652606+Tristen-Brown@users.noreply.github.com>
2024-10-25 11:19:51 -0500

    Added comments for functionality

--- tests/UnitTest.py
+++ tests/UnitTest.py
@@ -1,4 +1,5 @@
-
+#This tests whether the given list contains valid recipes saved into the recipe book. 
+#It calls isInRecipeBook to check entry by entry if each recipe in the list is in the recipe book.
 def validateList(list, recipeBook):
 	if(isInRecipeBook(list, recipeBook)):
 		print('List contains valid recipes')
@@ -8,6 +9,9 @@ def validateList(list, recipeBook):
 		print('List contains invalid recipes')
 		print(list)
 		print()
+		
+#This test checks whether the list has a valid length. Length can range from 1 to 255.
+#Lists of zero recipes are invalid and 256 or greater are invalid.
 def boundryList(list):
 	length = len(list)
 	if length > 0 and length <= 255:
@@ -22,7 +26,8 @@ def boundryList(list):
 		print('list cannot be longer than 255')
 		print(list)
 		print()
-		
+
+#Checks entry by entry for whether a recipe is in the recipe book.		
 def isInRecipeBook(list, recipeBook):
 	for i in list:
 		found = False
@@ -33,6 +38,7 @@ def isInRecipeBook(list, recipeBook):
 			return False
 	return True
 
+#Example test values to show functionality.
 if __name__ == '__main__':
 	validList = ['Chicken', 'Steak']
 	invalidList = ['Lego', 'Byte']
